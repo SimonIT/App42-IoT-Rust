b2a720357268a75f9517cc83de174a07b3316b31
SimonIT <simonit.orig@gmail.com>
2024-07-08 21:34:32 +0200

    Cross platform env joining

--- app42-iot-sys/build.rs
+++ app42-iot-sys/build.rs
@@ -5,22 +5,23 @@ use std::env;
 fn main() -> miette::Result<()> {
     let path = std::path::PathBuf::from("App42_IoT_SDK/V_1.0/App42");
 
-    let modules = path.read_dir()
+    let modules = env::join_paths(
+        path.read_dir()
         .unwrap()
         .filter(|e| e.is_ok())
-        .map(|e| e.unwrap().path().canonicalize().unwrap().to_str().unwrap().to_string())
-        .intersperse(":".to_string())
-        .collect::<String>();
+        .map(|e| e.unwrap().path().canonicalize().unwrap().into_os_string())
+    ).unwrap();
 
-    env::set_var("CPLUS_INCLUDE_PATH", format!("{}:{}", modules, env::var("CPLUS_INCLUDE_PATH").unwrap_or_default()));
+    unsafe { env::set_var("CPLUS_INCLUDE_PATH", modules); }
 
-    let mut b = autocxx_build::Builder::new("src/lib.rs", &[&path]).build()?;
+    let mut b = autocxx_build::Builder::new("src/lib.rs", [&path]).build()?;
     b.flag_if_supported("-frtti")
         .flag_if_supported("-std=c++14")
         .flag_if_supported("-fsigned-char")
         .flag_if_supported("-fexceptions")
         .file(path.join("Common/App42API.cpp"))
         .compile("autocxx-app42-iot");
+    println!("cargo:rerun-if-changed={}", path.display());
     println!("cargo:rerun-if-changed=src/lib.rs");
     Ok(())
 }
